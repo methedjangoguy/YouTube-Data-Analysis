dd5602f71fb7b4c5256f9e1602296562124bd4c4
Subhasish Swain <subhamsv96@gmail.com>
2023-07-07 12:07:44 +0530

    added two fucntions in ytworker.py

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ import log.log as log
 from config.config import configuration
 from ytworker.ytworker import *
 import pandas as pd
+import re
 
 _root_logger = log.setup_custom_logger("root")
 _yt_logger_ = log.setup_custom_logger("youtube")
 
-# Main Function: point of execution
+
+# Main Function
 def main():
     resultPath = configuration.get_property("resultPath")
     channelIDList = configuration.get_property("channelIDList")
-    channelID = ','.join(channelIDList)
+    channelID = ",".join(channelIDList)
     ytyoutubedata = getChannelStats(channelID)
     youtubedata = pd.DataFrame(ytyoutubedata)
     _root_logger.info(f"Displaying Multiple Youtube Channel Information.\n")
@@ -23,19 +25,48 @@ def main():
         try:
             ytvideodatadetails = getVideoDetails(ytvideoIDs)
             ytvideodata = pd.DataFrame(ytvideodatadetails)
-            _root_logger.info(f'------------------------')
+            _root_logger.info(f"------------------------")
             _root_logger.info(f"\nDisplaying Video data for Channel: {channelName}\n")
-            _root_logger.info(f'------------------------')
-            _root_logger.info(f'{ytvideodata}\n')
-            _root_logger.info(f"\nDisplaying Top 10 video's data for Channel: {channelName}\n")
-            top10videos = ytvideodata.sort_values(by='ViewCount', ascending=False).head(10)
-            _root_logger.info(f'{top10videos}')
-            _root_logger.info(f'------------------------')
-            ytvideodata.to_csv(f"{resultPath}VideoDetailsOf_{channelName}.csv", index=False)
-            _root_logger.info(f'File Created for {channelName}')
-            _root_logger.info(f'------------------------')
+            _root_logger.info(f"------------------------")
+            _root_logger.info(f"{ytvideodata}\n")
+            _root_logger.info(
+                f"\nDisplaying Top 10 video's data for Channel: {channelName}\n"
+            )
+            top10videos = ytvideodata.sort_values(by="ViewCount", ascending=False).head(
+                10
+            )
+            _root_logger.info(f"{top10videos}")
+            _root_logger.info(f"------------------------")
+            ytvideodata.to_csv(
+                f"{resultPath}/VideoDetailsOf_{channelName}.csv", index=False
+            )
+            _root_logger.info(f"File Created for {channelName}: {resultPath}/VideoDetailsOf_{channelName}.csv")
+            _root_logger.info(f"------------------------")
         except Exception as e:
-            _root_logger.error(f'Some exception occured while fetching Video data for channel {channelName}\n')
+            _root_logger.error(
+                f"Some exception occured while fetching Video data for channel {channelName}\n"
+            )
+
+    for channelid in channelIDList:
+        plplaylistdata = getplaylistID(channelid)
+        playlistdata = pd.DataFrame(plplaylistdata)
+
+        for index, row in playlistdata.iterrows():
+            try:
+                ytvideos = getvideonamesfromplaylist(row[0])
+                yt = pd.DataFrame(ytvideos)
+                if "/" in row[2]:
+                    row[2] = re.sub('/', '', row[2])
+
+                yt.to_csv(f"{resultPath}{row[1]}_{row[2]}_videos.csv", index=False)
+                _root_logger.info(yt)
+                _root_logger.info(f"------------------------")
+                _root_logger.info(f"File created file for {row[1]}_{row[2]}: {resultPath}{row[1]}_{row[2]}_videos.csv")
+                _root_logger.info(f"------------------------")
+            except Exception as e:
+                 _root_logger.error(
+                f"Some exception occured while fetching Video data for channel {row[1]}\n"
+            )
 
 if __name__ == "__main__":
     main()
--- ytworker/ytworker.py
+++ ytworker/ytworker.py
@@ -6,94 +6,168 @@ from config.config import configuration
 import logging
 import datetime
 
-warnings.filterwarnings('ignore')
+warnings.filterwarnings("ignore")
 _yt_logger_ = logging.getLogger("youtube")
 
 apiKey = configuration.get_property("apiKey")
-youtube = build('youtube','v3',developerKey=apiKey)
+youtube = build("youtube", "v3", developerKey=apiKey)
+
 
 # function to get youtube channel statistics.
 def getChannelStats(channelID):
-  dataframe = []
-  request = request = youtube.channels().list(
-        part='snippet,contentDetails,statistics',
+    dataframe = []
+    request = request = youtube.channels().list(
+        part="snippet,contentDetails,statistics",
         id=channelID,
     )
-  response  = request.execute()
-  try:
-      for i in range(len(response['items'])):
-          data = dict(
-          ChannelName = response['items'][i]['snippet']['title'],
-          Subscribers = response['items'][i]['statistics']['subscriberCount'],
-          Views = response['items'][i]['statistics']['viewCount'],
-          Videos = response['items'][i]['statistics']['videoCount'],
-          PlaylistID = response['items'][i]['contentDetails']['relatedPlaylists']['uploads'],
-          # ChannelID = response['items'][i]['id'],
-          Description = response['items'][i]['snippet']['description']
-          )
-          dataframe.append(data)
-      return dataframe
-  except Exception as e:
-        return f'Some error occured due to {e}'
+    response = request.execute()
+    try:
+        for i in range(len(response["items"])):
+            data = dict(
+                ChannelName=response["items"][i]["snippet"]["title"],
+                Subscribers=response["items"][i]["statistics"]["subscriberCount"],
+                Views=response["items"][i]["statistics"]["viewCount"],
+                Videos=response["items"][i]["statistics"]["videoCount"],
+                PlaylistID=response["items"][i]["contentDetails"]["relatedPlaylists"][
+                    "uploads"
+                ],
+                # ChannelID = response['items'][i]['id'],
+                Description=response["items"][i]["snippet"]["description"],
+            )
+            dataframe.append(data)
+        return dataframe
+    except Exception as e:
+        return f"Some error occured due to {e}"
 
 # function to get a specific channel's video IDs.
 def getVideoIDs(playlistid):
-  request = youtube.playlistItems().list(
-        part='contentDetails',
-        playlistId=playlistid,
-        maxResults = 50
+    request = youtube.playlistItems().list(
+        part="contentDetails", playlistId=playlistid, maxResults=50
     )
-  response = request.execute()
-  videoids = []
-  for i in range(len(response['items'])):
-        videoids.append(response['items'][i]['contentDetails']['videoId'])
+    response = request.execute()
+    videoids = []
+    for i in range(len(response["items"])):
+        videoids.append(response["items"][i]["contentDetails"]["videoId"])
 
-  nextpagetoken = response.get('nextPageToken')
-  morepages = True
-  while morepages:
-    if nextpagetoken is None:
-      morepages = False
-    else:
-      request = youtube.playlistItems().list(
-        part='contentDetails',
-        playlistId=playlistid,
-        maxResults = 50,
-        pageToken=nextpagetoken
-      )
-      response = request.execute()
-      for i in range(len(response['items'])):
-        videoids.append(response['items'][i]['contentDetails']['videoId'])
+    nextpagetoken = response.get("nextPageToken")
+    morepages = True
+    while morepages:
+        if nextpagetoken is None:
+            morepages = False
+        else:
+            request = youtube.playlistItems().list(
+                part="contentDetails",
+                playlistId=playlistid,
+                maxResults=50,
+                pageToken=nextpagetoken,
+            )
+            response = request.execute()
+            for i in range(len(response["items"])):
+                videoids.append(response["items"][i]["contentDetails"]["videoId"])
 
-      nextpagetoken = response.get('nextPageToken')
+            nextpagetoken = response.get("nextPageToken")
 
-  return videoids
+    return videoids
 
 # function to get a specific channel's video details based on video ID.
 def getVideoDetails(videIDs):
     videoStats = []
-    for i in range(0,len(videIDs),50):
+    for i in range(0, len(videIDs), 50):
         request = youtube.videos().list(
-            part = 'snippet,statistics',
-            id = ','.join(videIDs[i:i+50])
+            part="snippet,statistics", id=",".join(videIDs[i : i + 50])
         )
         response = request.execute()
         try:
-          for video in response['items']:
-              videoinfo = dict(
-                  Title = video['snippet']['title'],
-                  PublishedDate = pd.to_datetime(video['snippet']['publishedAt']).strftime("%d-%m-%Y"),
-                  ViewCount = video['statistics']['viewCount'],
-                  LikeCount = video['statistics']['likeCount'],
-                  CommentCount = video['statistics']['commentCount']
-              )
-              videoStats.append(videoinfo)
+            for video in response["items"]:
+                videoinfo = dict(
+                    Title=video["snippet"]["title"],
+                    PublishedDate=pd.to_datetime(
+                        video["snippet"]["publishedAt"]
+                    ).strftime("%d-%m-%Y"),
+                    ViewCount=video["statistics"]["viewCount"],
+                    LikeCount=video["statistics"]["likeCount"],
+                    CommentCount=video["statistics"]["commentCount"],
+                )
+                videoStats.append(videoinfo)
         except Exception as e:
-           videoinfo = dict(
-                  Title = video['snippet']['title'],
-                  PublishedDate = pd.to_datetime(video['snippet']['publishedAt']).strftime("%d-%m-%Y"),
-                  ViewCount = video['statistics']['viewCount'],
-                  LikeCount = "No Like Data found.",
-                  CommentCount = "No Comment Data found."
-              )
+            videoinfo = dict(
+                Title=video["snippet"]["title"],
+                PublishedDate=pd.to_datetime(video["snippet"]["publishedAt"]).strftime(
+                    "%d-%m-%Y"
+                ),
+                ViewCount=video["statistics"]["viewCount"],
+                LikeCount="No Like Data found.",
+                CommentCount="No Comment Data found.",
+            )
         videoStats.append(videoinfo)
     return videoStats
+
+# funtion to get a specific channel's playlist details based on channel ID.
+def getplaylistID(channel_id):
+    request = youtube.playlists().list(
+        part="snippet", channelId=channel_id, maxResults=50
+    )
+    response = request.execute()
+    playlistids = []
+    for i in range(len(response["items"])):
+        data = dict(
+            PlaylistID=response["items"][i]["id"],
+            Channel_Name = response["items"][i]["snippet"]["channelTitle"],
+            Playlist_Name = response["items"][i]["snippet"]["title"]
+        )
+        playlistids.append(data)
+    nextpagetoken = response.get("nextPageToken")
+    morepages = True
+    while morepages:
+        if nextpagetoken is None:
+            morepages = False
+        else:
+            request = youtube.playlists().list(
+                part="snippet", channelId=channel_id, maxResults=50,
+                pageToken=nextpagetoken,
+            )
+            response = request.execute()
+            for i in range(len(response["items"])):
+                data = dict(
+                      PlaylistID = response["items"][i]["id"],
+                      Channel_Name = response["items"][i]["snippet"]["channelTitle"],
+                      Playlist_Name = response["items"][i]["snippet"]["title"]
+                    )
+                playlistids.append(data)
+
+            nextpagetoken = response.get("nextPageToken")
+    return playlistids
+
+# function to get a playlist video details based on playlist ID.
+def getvideonamesfromplaylist(playlistid):
+    request = youtube.playlistItems().list(
+        part="snippet", playlistId=playlistid, maxResults=50
+    )
+    response = request.execute()
+    videonames = []
+    for i in range(len(response["items"])):
+        data = dict(
+            VideoName=response["items"][i]["snippet"]["title"],
+        )
+        videonames.append(data)
+
+    nextpagetoken = response.get("nextPageToken")
+    morepages = True
+    while morepages:
+        if nextpagetoken is None:
+            morepages = False
+        else:
+            request = youtube.playlistItems().list(
+                part="snippet",
+                playlistId=playlistid,
+                maxResults=50,
+                pageToken=nextpagetoken,
+            )
+            response = request.execute()
+            for i in range(len(response["items"])):
+                data = dict(
+                    VideoName=response["items"][i]["snippet"]["title"],
+                    )
+                videonames.append(data)
+            nextpagetoken = response.get("nextPageToken")
+    return videonames
